6ec8ab94108cb823c7dccb5b51021fc498f55628
Craig Newton <newtondev@gmail.com>
2018-06-13 17:39:17 +0200

    Added multiple possible solutions for reversestring exercise.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
+
+// MY SOLUTION - (Damn I am oldskool!)
+// function reverse(str) {  
+//   let reversed = '';
+//   for (let i = (str.length - 1); i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return reversed;
+// }
+
+// // SOLUTION #1
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+// SOLUTION #2
+// function reverse(str) {  
+//   let reversed = '';
+  
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// // SOLUTION #3 - prove that you know some advanced array functions ... reduce :)
+// function reverse(str) {
+//   return str.split('').reduce((rev, char) => char + rev, '');
+// }
